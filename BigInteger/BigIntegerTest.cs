e7ec7cca235b143192f804b454f4337852e16eba
Keiwan Donyagard <keiwan@gmx.de>
2016-12-14 17:51:45 +0100

    Initial commit

---
+++ BigInteger/BigIntegerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Keiwando.BigInteger {
+	
+	public class BigIntegerTest {
+
+		public static void runTests() {
+			
+			BigInteger number = new BigInteger ("1000");
+			var result = BigInteger.Add (number, new BigInteger("1"));
+			Console.Write (result);
+		}
+
+		static void Main(string[] args) {
+			BigIntegerTest.runTests ();
+		}
+	}
+}
+
